0d85b9f2cb14cca5205c65cbbb86f7997869d296
Amit Bothra <amit.bothra@outlook.com>
2019-01-28 05:59:21 -0600

    Added UserHeader Component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
@@ -17,7 +18,8 @@ class PostList extends React.Component {
             <div className="description">
               <h2>{post.title}</h2>
               <p>{post.body}</p>
-            </div>          
+            </div>
+            <UserHeader userId={post.userId} />          
           </div>
         </div>
       );
