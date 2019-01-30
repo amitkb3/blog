870bd2508510506ca175e4d25297487cc2101200
Amit Bothra <amit.bothra@outlook.com>
2019-01-29 21:46:48 -0600

    created fetchPostsAndUsers action creator

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { fetchUser } from '../actions';
-
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
+ 
   render() {
 
     const { user } = this.props;
@@ -28,8 +24,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(
-  mapStateToProps,
-  {
-    fetchUser 
-  })(UserHeader);
+  mapStateToProps)(UserHeader);
 
